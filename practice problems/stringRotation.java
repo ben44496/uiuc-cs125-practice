5d042fdbfbe6bf4baaa024654d37217d9b606d26
Jackson K <50086310+mersaults@users.noreply.github.com>
2020-08-13 19:58:12 -0400

    made readability changes to stringRotration

--- practice problems/stringRotation.java
+++ practice problems/stringRotation.java
@@ -1,21 +1,18 @@
-String rotateRight(String s, int rotate){
-        // int index = rotate % (s.length());
-        // return s.subString(index, s.length()) + s.subString(0, index);
-        char[] word = s.toCharArray();
-        char[] retur = new char[s.length()];
-        int index=0;
-        // This part does the switch
-        for(int i =s.length()-rotate%(s.length()); i<s.length(); i++){
-            retur[index] = word[i];
-            index++;
-            //count++;
-        }
-        // This part does the fill
-        int ind=0;
-        for(int j=index; j<s.length(); j++){
-            retur[index] = word[ind];
-            index++;
-            ind++;
-        }
-        return new String(retur);
+public String rotateRight(String s, int rotate) {
+    char[] dummy = s.toCharArray();
+    char[] output = new char[dummy.length];
+    
+    // copy dummy into output array. note that arrays in java are by reference, not by value.
+    for (int i = 0; i < output.length; i++) {
+        output[i] = dummy[i];
+    }
+    
+    // rearrange output array
+    for (int i = 0; i < output.length; i++) {
+        output[(i + rotate) % output.length] = dummy[i];
+    }
+    
+    // convert array to string
+    String stringoutput = new String(output);
+    return stringoutput;
 }
