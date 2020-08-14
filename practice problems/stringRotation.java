db802a608c3ccd6b4d36421ab9190c31e1c381dd
Jackson K <50086310+mersaults@users.noreply.github.com>
2020-08-13 20:36:36 -0400

    remove redundant loop

--- practice problems/stringRotation.java
+++ practice problems/stringRotation.java
@@ -2,11 +2,6 @@ public String rotateRight(String s, int rotate) {
     char[] dummy = s.toCharArray();
     char[] output = new char[dummy.length];
     
-    // copy dummy into output array. note that arrays in java are by reference, not by value.
-    for (int i = 0; i < output.length; i++) {
-        output[i] = dummy[i];
-    }
-    
     // rearrange output array
     for (int i = 0; i < output.length; i++) {
         output[(i + rotate) % output.length] = dummy[i];
