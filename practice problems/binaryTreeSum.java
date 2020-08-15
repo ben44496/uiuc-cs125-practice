d9c6438b500a4612966995c384352d297fc8913e
Manas Anand <maanas.anand@gmail.com>
2020-08-14 21:47:57 -0700

    Update binaryTreeSum.java

--- practice problems/binaryTreeSum.java
+++ practice problems/binaryTreeSum.java
@@ -1,17 +1,24 @@
-public class YourBinaryTree extends BinaryTree{
-    
-    
-    
-    @Override
-    protected int sum(){
-        return summation(super.root);
+ublic class YourBinaryTree extends BinaryTree {
+   
+    // Overriden method 
+    public int sum() {
+        return sum(root);
     }
     
-    private Integer summation(Node current){
-        if(current!=null){
-            Integer myValue = (Integer) current.value;
-            return myValue.intValue() + summation(current.left) + summation(current.right);
+    private int sum (Node current) {
+        /* 
+            This is our base case. We need to return 0 when there are no more leaves left in the binary tree.
+        */
+        if (current == null) {
+            return 0;
         }
-        return (Integer) 0;
+        /* 
+            This is used to wrap  current's value as an Integer, since value is currently of type Object. 
+        */
+        Integer newValue = (Integer) current.value;
+        /* 
+            This is the recursive portion of the code, where we keep going down the binary tree, adding the value of each leaf.
+        */
+        return newValue.intValue() + sum(current.left) + sum(current.right);
     }
 }
