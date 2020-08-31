8ebe601e1f376d1a55c6a539382b8ba494455050
David Lin <dlin2028@gmail.com>
2020-08-31 16:46:06 -0700

    fixed curly braces to be consistent

--- practice exam/comparableFerret.java
+++ practice exam/comparableFerret.java
@@ -1,20 +1,18 @@
 public class Ferret implements Comparable{
     private double length=0;
     
-    public Ferret(double len){
+    public Ferret(double len) {
         length = len;
     }
-    public void setLength(double len)
-    {
+    public void setLength(double len) {
         length=len;
     }
-    public double getLength()
-    {
+    public double getLength() {
         return length;
     }
     
     @Override
-    public int compareTo(Object second){
+    public int compareTo(Object second) {
         if(second!=null && second instanceof Ferret){
             return (int)(this.length - ((Ferret)second).getLength());
         }
--- practice exam/rightTriangleClass.java
+++ practice exam/rightTriangleClass.java
@@ -1,24 +1,22 @@
 public class RightTriangle extends Shape{
     private int length;
     
-    public RightTriangle(int s){
+    public RightTriangle(int s) {
         super("righttriangle");
         length = s;
     }
     
-    public int getLength()
-    {
+    public int getLength() {
         return length;
     }
 
-    public double area()
-    {
+    public double area() {
         //the d at the end means it is a double constant
         return  0.5d * (double)length * (double)length;
     }
     
     @Override
-    public boolean equals(Object other){
+    public boolean equals(Object other) {
         if(other!=null && other instanceof RightTriangle){
             return ((RightTriangle)other).getLength() == length;
         }
